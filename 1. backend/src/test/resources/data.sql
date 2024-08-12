dfc356b453099b4b18aae28e059ea941c53c2daf
Ramyr <myrreg@protonmail.com>
2024-08-12 10:27:51 +0800

    added Put method and schema.sql and data.sql for ease of testing and data manipulation

---
+++ 1. backend/src/test/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO to_do(id, title, description, status) 
+VALUES
+(101, 'Cook', 'Cook Adobo', 'PENDING'),
+(102, 'Exercise', 'Morning run', 'COMPLETED'),
+(103, 'Read', 'Read a book', 'PENDING');
